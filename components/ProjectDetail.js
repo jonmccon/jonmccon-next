4ed28e6a5bb8759abf7208d8077764e4ddf1ffea
Jonny McConnell <jonmccon@gmail.com>
2024-01-07 15:51:09 -0800

    stlying for project detail page

--- components/ProjectDetail.js
+++ components/ProjectDetail.js
@@ -2,27 +2,40 @@ import Image from 'next/image'
 
 export default function ProjectDetail({ projectData }) {
   return (
-    <div>
-      <h1>{projectData.title}</h1>
-      {projectData.sections.map((section, index) => (
-        <div key={index}>
-          <h2>{section.subtitle}</h2>
-          {section.description.map((desc, descIndex) => (
-            <p key={descIndex}>{desc}</p>
+    <section className="bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+        <h1 className="text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+          {projectData.title}
+        </h1>
+      </div>
+      <div className="bg-[#F1F1F1] -mt-10 dark:bg-gray-900">
+        <div className="text-container max-w-6xl mx-auto pt-20">
+          {projectData.sections.map((section, index) => (
+            <div key={index}>
+              <h2 className="leading-loose text-2xl md:text-4xl font-semibold mx-4" style={{ lineHeight: "3rem" }}>
+                {section.subtitle}
+              </h2>
+              {section.description.map((desc, descIndex) => (
+                <p key={descIndex} className="text-xl text-gray-700 mb-4 dark:text-gray-300">
+                  {desc}
+                </p>
+              ))}
+              {section.images.map((img, imgIndex) => (
+                <Image 
+                  key={imgIndex} 
+                  src={img} 
+                  width={1000} 
+                  height={3000} 
+                  placeholder="blur"
+                  blurDataURL="/avatar.jpg" // find a smaller image to use here
+                  className="my-4"
+                />
+              ))}
+            </div>
           ))}
-          {section.images.map((img, imgIndex) => (
-            <Image 
-              key={imgIndex} 
-              src={img} 
-              width={1000} 
-              height={3000} 
-              placeholder="blur"
-              blurDataURL="/avatar.jpg" // find a smaller image to use here
-            />
-          ))}
+          {/* Render other project data... */}
         </div>
-      ))}
-      {/* Render other project data... */}
-    </div>
+      </div>
+    </section>
   )
 }
